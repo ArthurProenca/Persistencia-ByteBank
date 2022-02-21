0b3df7acab649256d55a76c23f33525d2cf24d72
Arthur Proença <m270800>
2022-02-21 20:20:12 -0300

    Modularização do código, criação do container de botão e alinhamentos Cross (x) e Axis(y).

--- lib/main.dart
+++ lib/main.dart
@@ -6,21 +6,56 @@ class BytebankApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Dashboard'),
-        ),
-        body: Column(
-          children: <Widget>[
-            Image.asset('images/bytebank_logo.png'),
-            Container(
-                color: Colors.green,
-                child: Column(children: <Widget>[
-                  Icon(Icons.people),
-                  Text('Contacts'),
-                ])),
-          ],
-        ),
+      theme: ThemeData(
+          primaryColor: Colors.green[900],
+          buttonTheme: ButtonThemeData(
+            buttonColor: Colors.blueAccent[700],
+            textTheme: ButtonTextTheme.primary,
+          ),
+          colorScheme: ColorScheme.fromSwatch()
+              .copyWith(secondary: Colors.blueAccent[700])),
+      home: Dashboard(),
+    );
+  }
+}
+
+class Dashboard extends StatelessWidget {
+  const Dashboard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Dashboard'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        //Ambos podem ser instânciados por esse enum.
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset('images/bytebank_logo.png'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+                padding: const EdgeInsets.all(8.0),
+                //Em todo o contâiner.
+                height: 100,
+                width: 150,
+                color: Theme.of(context).primaryColor,
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const <Widget>[
+                      Icon(Icons.people, color: Colors.white, size: 32.0),
+                      Text('Contacts',
+                          style:
+                              TextStyle(color: Colors.white, fontSize: 16.0)),
+                    ])),
+          ),
+        ],
       ),
     );
   }
